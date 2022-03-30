b0419075e1a8aa1c231529c9b9f709b22192208a
luanheider <luanheider@hotmail.com>
2022-03-30 13:30:24 -0400

    submitting index application

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+    return (
+    <div>
+        <img src="/img/people.svg" alt="People" />
+        <form>
+          <textarea
+            name="areatext"
+            id="areatext"
+            cols="30"
+            rows="10">
+          </textarea>
+          <button>
+              Comentar
+          </button>
+        </form>
+    </div>
+  )
+}
+
+export default App;
